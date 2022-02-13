354e7c9d2d60980381809f3919e0c9080ff16897
KarinMunoz <karinstefa@gmail.com>
2022-02-12 23:38:29 -0500

    se agregaron los endpoints para registrar administradores y validar administradores

--- backend/app.py
+++ backend/app.py
@@ -6,30 +6,28 @@ from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
 from flask_cors import CORS
 
-from backend.model_db import Modelo
-
 app = Flask(__name__)
 CORS(app)
 
 # Inicilizacion de base de datos
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/project01"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:123456@localhost:5432/project01"
 # base de datos
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
 # Crear Clases y esquemas
-class Administrador(db.Model):
+class Administradores(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nombres = db.Column(db.String(50) )
     apellidos = db.Column(db.String(50) )
     correo = db.Column(db.String(50) )
-    contrasena = db.Column(db.String(255) )
+    password = db.Column(db.String(255) )
 
 class Administrador_Schema(ma.Schema):
     class Meta:
-        fields = ("id", "nombres", "apellidos", "correo", "contrasena")
+        fields = ("id", "nombres", "apellidos", "correo", "password")
 
 administrador_schema = Administrador_Schema()
 administradores_schema = Administrador_Schema(many = True)
@@ -39,21 +37,31 @@ administradores_schema = Administrador_Schema(many = True)
 api = Api(app)
 
 # Acciones GET/POST/PUT/DELETE
-class CRUDAdministrador(Resource):
+class RegistrarAdministrador(Resource):
     # Insertar Administrador
     def post(self):
-        nuevo_administrador = Administrador(
+        nuevo_administrador = Administradores(
             nombres = request.json['nombres'],
             apellidos = request.json['apellidos'],
             correo = request.json['correo'],
-            contrasena = request.json['contrasena']
+            password = request.json['password']
         )
         db.session.add(nuevo_administrador)
         db.session.commit()
-        return {'message','Administrador creado exitosamente.'}
+        return {'message':'Administrador creado exitosamente.'}
 
+class ValidarAdministrador(Resource):
+    def post(self):
+        if 'correo' in request.json:
+            if 'password' in request.json:
+                admins = Administradores.query.filter_by(correo=request.json['correo']).all()
+                for admin in admins:
+                    if admin.password == request.json['password']:
+                        return {'success':'true','id':admin.id}
+        return {'success':'false'}
 # Endpoints
-api.add_resource(CrearAdministrador, '/administrador')
+api.add_resource(RegistrarAdministrador, '/administrador')
+api.add_resource(ValidarAdministrador, '/validar_administrador')
 
 
 @app.route("/")
