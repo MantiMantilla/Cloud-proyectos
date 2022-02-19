6c6c2ffdcf093127d38975b526ff0153f4025725
Karin Munoz <ksmunoz@superservicios.gov.co>
2022-02-18 22:22:06 -0500

    ajustes de servicio

--- service/servicio_mp3.py
+++ service/servicio_mp3.py
@@ -30,7 +30,8 @@ for r in result_set:
 
         # enviar correo
         try:
-            server = smtplib.SMTP_SSL('smtp.gmail.com', 587)
+            server = smtplib.SMTP('smtp.gmail.com', 25)
+            server.starttls()
             server.ehlo()
             server.login(gmail_user, gmail_password)
             print("Loged ok")
